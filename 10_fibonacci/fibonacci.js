7fa080c0dfe2250a2968399fd651cb708a104570
Alice Jackson <alicejj92@gmail.com>
2023-06-30 15:13:17 +0100

    fibonacci and findTheTitle challenges completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  if (n < 0) return "OOPS";
+  fib = [0, 1];
+  for (let i = 1; i < n; i++) {
+    fib.push(fib[i] + fib[i - 1]);
+  };
+  return fib[n]
 };
 
 // Do not edit below this line
