d2e244e95c652386b949d03080b78b70b8b496c9
Alice Jackson <alicejj92@gmail.com>
2023-06-30 14:45:24 +0100

    palindrome challenge complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const processedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+  return processedString.split('').reverse().join('') == processedString;
 };
+
+console.log(palindromes('racecar'))
 
 // Do not edit below this line
 module.exports = palindromes;
