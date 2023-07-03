b3057a746f2ca4fe7e88a473275b2d7af9bac492
Alice Jackson <alicejj92@gmail.com>
2023-07-03 10:37:38 +0100

    findTheOldest challenge complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  return people.reduce((oldest, current) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+    return oldestAge < currentAge ? current : oldest;
+  });
 };
+
+const getAge = function(yearOfBirth, yearOfDeath) {
+  if (!yearOfDeath) {
+    yearOfDeath = new Date().getFullYear()
+  };
+  return yearOfDeath - yearOfBirth
+};
+
+console.log(findTheOldest(people).name)
 
 // Do not edit below this line
 module.exports = findTheOldest;
